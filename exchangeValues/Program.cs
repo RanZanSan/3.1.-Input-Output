df721983386796042bb08849d7c852c5c4ca9edf
Andrey Rasskazov <ranzansan@gmail.com>
2021-05-21 13:35:53 +0300

    Добавьте файлы проекта.

---
+++ exchangeValues/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exchangeValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a;
+
+            a = b;
+            b = c;
+
+            Console.WriteLine(a + " " + b);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ squareNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace squareNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int squareNumber = number * number;
+
+            Console.WriteLine(squareNumber);
+            Console.ReadKey();
+        }
+    }
+}
